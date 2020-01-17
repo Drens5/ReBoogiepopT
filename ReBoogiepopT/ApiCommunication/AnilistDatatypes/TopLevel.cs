ffe2b0a1972cd2998e19df90d083552b69bd246d
Drens5 <drens1o5@gmail.com>
2020-01-17 12:38:19 +0100

    Add project files.

---
+++ ReBoogiepopT/ApiCommunication/AnilistDatatypes/TopLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ReBoogiepopT.ApiCommunication.AnilistDatatypes
+{
+    /// <summary>
+    /// Class defines the entry point of a Json response.
+    /// </summary>
+    class TopLevel
+    { 
+    }
+}
